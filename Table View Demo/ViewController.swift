6910bc749948b99535a2a645edfccf58865079a1
Aashirwad Sinha <aashirwad@ratibpal.com>
2019-11-07 16:52:10 +0530

    Initial Commit

---
+++ Table View Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View Demo
+//
+//  Created by Aashirwad Sinha on 11/7/19.
+//  Copyright © 2019 Credit Suisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
